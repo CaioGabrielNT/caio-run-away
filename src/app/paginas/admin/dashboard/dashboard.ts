189ac16b3db61c4e53841d954741c33ba581d66c
CaioGabrielNT <antunes.caiogabriel@gmail.com>
2026-06-18 10:01:36 -0300

    feat: implement dashboard component with moda loading functionality

--- src/app/paginas/admin/dashboard/dashboard.ts
+++ src/app/paginas/admin/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ModaService } from '../../../core/services/moda';
+import { ModaModel } from '../../../core/models/moda';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+
+private readonly modaService = inject(ModaService);
+
+moda =signal<ModaModel[]>([]);
+isloading = signal(true);
+
+  ngOnInit(): void {
+    this.loadModa();
+  }
+
+  private loadModa(): void {
+    this.modaService.getAll().subscribe({
+      next: (data) => {
+        this.moda.set(data);
+        this.isloading.set(false);
+      },
+      error: (error) => {
+        console.error('Error al cargar la moda:', error);
+        this.isloading.set(false);
+      }
+    });
+
+}
+
+}
